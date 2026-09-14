a1bc7eacc946c4b1964d31bc67640941812111ab
Prithul-the-creator <themagicman932@gmail.com>
2026-09-14 01:25:39 -0400

    v2

--- movies/views.py
+++ movies/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def show(request, id):
     movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    reviews = Review.objects.filter(movie=movie, reported = False)
 
     template_data = {}
     template_data['title'] = movie.name
@@ -61,3 +61,24 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+
+from django.contrib import messages
+@login_required
+def report(request, id, review_id):
+    
+    review = get_object_or_404(Review, id=review_id, user=request.user)
+    if review.user != request.user:
+        review.reported = True
+        review.save()
+    else:
+        messages.error(request, "Document deleted.")
+    
+    return redirect('movies.show', id=id)
+    
+    
+
+
+    
+    
+    
+    
